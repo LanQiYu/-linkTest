4f9cb137d1f090fbf380a15d77a0484ccc77a415
gaoyong <2064522063@qq.com>
2022-12-28 16:32:49 +0800

    作品展示

---
+++ js/hover.js
@@ -0,0 +1,167 @@
+$(document).ready(function(){
+    $('#a').hover(function() {
+        $("#b").css('display', 'block');
+    }, function() {
+       $("#b").css('display', 'none');
+    });
+
+    $("#b").hover(function() {
+       $(this).css('display', 'block');;
+    }, function() {
+       $(this).css('display', 'none');
+    });
+})
+
+$(document).ready(function(){
+    $('#a2').hover(function() {
+        $("#b2").css('display', 'block');
+    }, function() {
+       $("#b2").css('display', 'none');
+    });
+
+    $("#b2").hover(function() {
+       $(this).css('display', 'block');;
+    }, function() {
+       $(this).css('display', 'none');
+    });
+})
+
+$(document).ready(function(){
+    $('#a3').hover(function() {
+        $("#b3").css('display', 'block');
+    }, function() {
+       $("#b3").css('display', 'none');
+    });
+
+    $("#b3").hover(function() {
+       $(this).css('display', 'block');;
+    }, function() {
+       $(this).css('display', 'none');
+    });
+})
+
+$(document).ready(function(){
+    $('#a4').hover(function() {
+        $("#b4").css('display', 'block');
+    }, function() {
+       $("#b4").css('display', 'none');
+    });
+
+    $("#b4").hover(function() {
+       $(this).css('display', 'block');;
+    }, function() {
+       $(this).css('display', 'none');
+    });
+})
+
+$(document).ready(function(){
+    $('#a5').hover(function() {
+        $("#b5").css('display', 'block');
+    }, function() {
+       $("#b5").css('display', 'none');
+    });
+
+    $("#b5").hover(function() {
+       $(this).css('display', 'block');;
+    }, function() {
+       $(this).css('display', 'none');
+    });
+})
+
+$(document).ready(function(){
+    $('#a6').hover(function() {
+        $("#b6").css('display', 'block');
+    }, function() {
+       $("#b6").css('display', 'none');
+    });
+
+    $("#b6").hover(function() {
+       $(this).css('display', 'block');;
+    }, function() {
+       $(this).css('display', 'none');
+    });
+})
+
+$(document).ready(function(){
+    $('#a7').hover(function() {
+        $("#b7").css('display', 'block');
+    }, function() {
+       $("#b7").css('display', 'none');
+    });
+
+    $("#b7").hover(function() {
+       $(this).css('display', 'block');;
+    }, function() {
+       $(this).css('display', 'none');
+    });
+})
+
+$(document).ready(function(){
+    $('#a8').hover(function() {
+        $("#b8").css('display', 'block');
+    }, function() {
+       $("#b8").css('display', 'none');
+    });
+
+    $("#b8").hover(function() {
+       $(this).css('display', 'block');;
+    }, function() {
+       $(this).css('display', 'none');
+    });
+})
+
+$(document).ready(function(){
+    $('#a9').hover(function() {
+        $("#b9").css('display', 'block');
+    }, function() {
+       $("#b9").css('display', 'none');
+    });
+
+    $("#b9").hover(function() {
+       $(this).css('display', 'block');;
+    }, function() {
+       $(this).css('display', 'none');
+    });
+})
+
+$(document).ready(function(){
+    $('#a10').hover(function() {
+        $("#b10").css('display', 'block');
+    }, function() {
+       $("#b10").css('display', 'none');
+    });
+
+    $("#b10").hover(function() {
+       $(this).css('display', 'block');;
+    }, function() {
+       $(this).css('display', 'none');
+    });
+})
+
+$(document).ready(function(){
+    $('#a11').hover(function() {
+        $("#b11").css('display', 'block');
+    }, function() {
+       $("#b11").css('display', 'none');
+    });
+
+    $("#b11").hover(function() {
+       $(this).css('display', 'block');;
+    }, function() {
+       $(this).css('display', 'none');
+    });
+})
+
+$(document).ready(function(){
+    $('#a12').hover(function() {
+        $("#b12").css('display', 'block');
+    }, function() {
+       $("#b12").css('display', 'none');
+    });
+
+    $("#b12").hover(function() {
+       $(this).css('display', 'block');;
+    }, function() {
+       $(this).css('display', 'none');
+    });
+})
